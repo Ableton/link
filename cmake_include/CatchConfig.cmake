465c16a3ee375866ed79900526935fa29b684a25
fgo <fgo@ableton.com>
2017-09-26 15:50:52 +0200

    Add catch single-header

--- cmake_include/CatchConfig.cmake
+++ cmake_include/CatchConfig.cmake
@@ -2,5 +2,5 @@ add_library(Catch::Catch IMPORTED INTERFACE)
 
 set_property(TARGET Catch::Catch APPEND PROPERTY
   INTERFACE_INCLUDE_DIRECTORIES
-  ${CMAKE_SOURCE_DIR}/modules/catch/single_include
+  ${CMAKE_SOURCE_DIR}/third_party/catch
 )
