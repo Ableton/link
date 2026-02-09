9a566c7fc1f358817fec772f532e78f5cbff945d
fgo <fgo@ableton.com>
2026-02-09 15:59:33 +0100

    Add util::Locked

---
+++ include/ableton/util/Locked.hpp
@@ -0,0 +1,58 @@
+/* Copyright 2025, Ableton AG, Berlin. All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  If you would like to incorporate Link into a proprietary software application,
+ *  please contact <link-devs@ableton.com>.
+ */
+
+
+#pragma once
+
+#include <mutex>
+
+namespace ableton
+{
+namespace util
+{
+
+template <typename T>
+struct Locked
+{
+public:
+  Locked(const T& val)
+    : mVal(val)
+  {
+  }
+
+  T read() const
+  {
+    std::lock_guard lock(mMutex);
+    return mVal;
+  }
+
+  template <typename Handler>
+  void update(Handler handler)
+  {
+    std::lock_guard lock(mMutex);
+    handler(mVal);
+  }
+
+private:
+  T mVal;
+  mutable std::mutex mMutex;
+};
+
+} // namespace util
+} // namespace ableton
